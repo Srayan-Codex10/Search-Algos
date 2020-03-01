5b31459d54f4dc86826be44bb4577afc9921ba2f
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2020-03-01 15:42:27 +0530

    sample input added

--- bfs.py
+++ bfs.py
@@ -73,3 +73,88 @@ def main():
     createGraphBfs()
 
 main()
+# SAMPLE INPUT & OUTPUT 
+# Enter number of nodes: 
+# 15
+# Enter number of edges
+# 14
+# Enter nodes
+# A
+# B
+# C
+# D
+# E
+# F
+# G
+# H
+# I
+# J
+# K
+# L
+# M
+# N
+# O
+# Enter edges
+# enter the node-pairs for edge
+# AB
+# enter the node-pairs for edge
+# AC
+# enter the node-pairs for edge
+# AD
+# enter the node-pairs for edge
+# BE
+# enter the node-pairs for edge
+# BF
+# enter the node-pairs for edge
+# BG
+# enter the node-pairs for edge
+# CJ
+# enter the node-pairs for edge
+# JK
+# enter the node-pairs for edge
+# JL
+# enter the node-pairs for edge
+# DM
+# enter the node-pairs for edge
+# MN
+# enter the node-pairs for edge
+# NH
+# enter the node-pairs for edge
+# NI
+# enter the node-pairs for edge
+# NO
+# {'A': ['B', 'C', 'D'], 'B': ['A', 'E', 'F', 'G'], 'C': ['A', 'J'], 'D': ['A', 'M'], 'E': ['B'], 'F': ['B'], 'G': ['B'], 'H': ['N'], 'I': ['N'], 'J': ['C', 'K', 'L'], 'K': ['J'], 'L': ['J'], 'M': ['D', 'N'], 'N': ['M', 'H', 'I', 'O'], 'O': ['N']}
+#  [('A', 'B'), ('A', 'C'), ('A', 'D'), ('B', 'E'), ('B', 'F'), ('B', 'G'), ('C', 'J'), ('J', 'K'), ('J', 'L'), ('D', 'M'), ('M', 'N'), ('N', 'H'), ('N', 'I'), ('N', 'O')]
+# Enter Source Node: A
+# BFS
+
+# deque(['A'])
+# Visited ['A']
+# deque(['B', 'C', 'D'])
+# Visited ['A', 'B']
+# deque(['C', 'D', 'E', 'F', 'G'])
+# Visited ['A', 'B', 'C']
+# deque(['D', 'E', 'F', 'G', 'J'])
+# Visited ['A', 'B', 'C', 'D']
+# deque(['E', 'F', 'G', 'J', 'M'])
+# Visited ['A', 'B', 'C', 'D', 'E']
+# deque(['F', 'G', 'J', 'M'])
+# Visited ['A', 'B', 'C', 'D', 'E', 'F']
+# deque(['G', 'J', 'M'])
+# Visited ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+# deque(['J', 'M'])
+# Visited ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'J']
+# deque(['M', 'K', 'L'])
+# Visited ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'J', 'M']
+# deque(['K', 'L', 'N'])
+# Visited ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'J', 'M', 'K']
+# deque(['L', 'N'])
+# Visited ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'J', 'M', 'K', 'L']
+# deque(['N'])
+# Visited ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'J', 'M', 'K', 'L', 'N']
+# deque(['H', 'I', 'O'])
+# Visited ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'J', 'M', 'K', 'L', 'N', 'H']
+# deque(['I', 'O'])
+# Visited ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'J', 'M', 'K', 'L', 'N', 'H', 'I']
+# deque(['O'])
+# Visited ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'J', 'M', 'K', 'L', 'N', 'H', 'I', 'O']
